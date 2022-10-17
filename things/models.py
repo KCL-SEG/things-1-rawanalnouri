3453641dae7ca1a8ffdf882194ec810fe7d76d76
Rawan Alnouri <alnourirawan@gmail.com>
2022-10-17 11:22:26 +0100

    Amending restrictions to Thing model attributes

--- things/models.py
+++ things/models.py
@@ -6,11 +6,11 @@ from django.core.validators import MinValueValidator
 
 class Thing(Model):
     name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(max_length=120, blank=True)
+    description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
-        default=1,
+        default=0,
         validators=[
             MaxValueValidator(100),
-            MinValueValidator(1)
+            MinValueValidator(0)
         ]
      )
